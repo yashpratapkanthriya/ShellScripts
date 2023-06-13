8011e092a88c8637573c51fadeae1add4e978ddd
YashPratap Kantharia <32524804+yashpratapkanthriya@users.noreply.github.com>
2023-06-13 13:19:37 +0530

    Create CreatingArray.sh

---
+++ Practice/CreatingArray.sh
@@ -0,0 +1,10 @@
+name[0]="a"
+name[1]="b"
+name[2]="c"
+
+echo " name with index 0 $name[0]"
+echo " name with index 1 $name[1]"
+echo " name with index 2 $name[2]"
+
+echo " acess all name using * $name[*]"
+echo " acess all name using @ $name[@]"
